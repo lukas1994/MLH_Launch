b3ccf6c133101a9c4b358170c0ae77ce8a4c8206
Lukas Köbis <lukaskoebis@gmail.com>
2014-10-05 02:52:22 +0100

    data

--- mendeley/test.py
+++ mendeley/test.py
@@ -1,16 +1,8 @@
-#email = "taimurlukas@gmail.com"
-#password = "mlhmlh"
-clientID = "938"
-clientSecret = "bkzasRJgHn9tcvT3"
-
 from mendeley import Mendeley
+import json
 
-mendeley = Mendeley(clientID, clientSecret)
+mendeley = Mendeley("938", "bkzasRJgHn9tcvT3")
 session = mendeley.start_client_credentials_flow().authenticate()
-
-#doi = raw_input('Enter a DOI: ')
-
-#doc = session.catalog.by_identifier(doi=doi, view='all')
 
 highest_index = 0
 visited = []
@@ -22,8 +14,6 @@ def search(author, dist = 0):
 
 	author = author.encode('utf-8')
 
-	print dist, author
-
 	if dist >= 4:
 		return
 	if author in visited:
@@ -32,7 +22,7 @@ def search(author, dist = 0):
 
 	nodes.append({'name': author, 'group': 1})
 
-	docs = map(lambda x: map(lambda a: a.last_name, x.authors), session.catalog.advanced_search(author=author).list(10).items)
+	docs = map(lambda x: map(lambda a: a.last_name, x.authors), session.catalog.advanced_search(author=author).list(100).items)
 	coauthors = []
 
 	for doc in docs:
@@ -41,30 +31,30 @@ def search(author, dist = 0):
 			continue
 		coauthors += others
 
+	coauthors = coauthors[:8]
+
+	my_idx = highest_index
+	highest_index += 1
 	for co in set(coauthors):
 		idx = search(co, dist+1)
 		if idx:
-			links.append({'source': highest_index, 'target': idx, 'value': 1})
+			links.append({'source': my_idx, 'target': idx, 'value': 1, 'weight':1})
 
-	highest_index += 1
+	return my_idx
 
-	return highest_index - 1
 
-author1 = "Erdoes"
-search(author1)
+from flask import Flask
+app = Flask(__name__)
 
-def o_str(o):
-	s = "{"
-	s += ",".join(["\"" + key + "\": \"" + str(value) + "\"" for key, value in o.iteritems()])
-	s += "}"
-	return s
-def l_str(l):
-	s = "["
-	s += ",".join([o_str(o) for o in l])
-	s += "]"
-	return s
-import json
-print json.dumps({"nodes": nodes, "links": links})
-#print "{\"nodes\": "+l_str(nodes)+",\"links\":"+l_str(links)+"}"
+@app.route('/data/<author>')
+def hello_world(author):
+	print author
+	search(author)
+	return json.dumps({"nodes": nodes, "links": links})
+
+#url_for('static', filename='index.html')
+
+if __name__ == '__main__':
+    app.run()
 
 
