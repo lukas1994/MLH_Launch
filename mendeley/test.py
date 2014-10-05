624220f170ced11592fa0dc149b2bd18e6b317b6
Lukas Köbis <lukaskoebis@gmail.com>
2014-10-05 01:13:04 +0100

    python backend

--- mendeley/test.py
+++ mendeley/test.py
@@ -1,5 +1,5 @@
-email = "taimurlukas@gmail.com"
-password = "mlhmlh"
+#email = "taimurlukas@gmail.com"
+#password = "mlhmlh"
 clientID = "938"
 clientSecret = "bkzasRJgHn9tcvT3"
 
@@ -8,12 +8,63 @@ from mendeley import Mendeley
 mendeley = Mendeley(clientID, clientSecret)
 session = mendeley.start_client_credentials_flow().authenticate()
 
-doi = raw_input('Enter a DOI: ')
+#doi = raw_input('Enter a DOI: ')
 
-doc = session.catalog.by_identifier(doi=doi, view='all')
+#doc = session.catalog.by_identifier(doi=doi, view='all')
 
-print doc.link
+highest_index = 0
+visited = []
+nodes = []
+links = []
 
-#print '"%s" has %s readers.' % (doc.title, doc.reader_count)
+def search(author, dist = 0):
+	global highest_index
+
+	author = author.encode('utf-8')
+
+	print dist, author
+
+	if dist >= 4:
+		return
+	if author in visited:
+		return
+	visited.append(author)
+
+	nodes.append({'name': author, 'group': 1})
+
+	docs = map(lambda x: map(lambda a: a.last_name, x.authors), session.catalog.advanced_search(author=author).list(10).items)
+	coauthors = []
+
+	for doc in docs:
+		others = filter(lambda a: False if a == author else True, doc)
+		if len(others) == len(doc):
+			continue
+		coauthors += others
+
+	for co in set(coauthors):
+		idx = search(co, dist+1)
+		if idx:
+			links.append({'source': highest_index, 'target': idx, 'value': 1})
+
+	highest_index += 1
+
+	return highest_index - 1
+
+author1 = "Erdoes"
+search(author1)
+
+def o_str(o):
+	s = "{"
+	s += ",".join(["\"" + key + "\": \"" + str(value) + "\"" for key, value in o.iteritems()])
+	s += "}"
+	return s
+def l_str(l):
+	s = "["
+	s += ",".join([o_str(o) for o in l])
+	s += "]"
+	return s
+import json
+print json.dumps({"nodes": nodes, "links": links})
+#print "{\"nodes\": "+l_str(nodes)+",\"links\":"+l_str(links)+"}"
 
 
