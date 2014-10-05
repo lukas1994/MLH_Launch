2d987d57d5fc9c7c6685ca1d1f795286d9a006d5
Lukas Köbis <lukaskoebis@gmail.com>
2014-10-05 03:53:22 +0100

    rc1

--- mendeley/server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var connect = require('connect');
-
-connect()
-    .use(connect.static('.'))
-    .listen(3000);
-
-console.log('Listening on port 3000.');
--- mendeley/test.py
+++ mendeley/test.py
@@ -59,7 +59,7 @@ def search(author):
 
 
 from flask import Flask
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
 def root():
